bebc9692bb500e3ee97f224eeba9ef9555be7390
Alex Metzger <asm@asm.io>
2020-03-24 14:23:17 -0400

    infra: disable `www.`

--- infra/main.tf
+++ infra/main.tf
@@ -252,26 +252,30 @@ resource "aws_route53_record" "nakedv6" {
   }
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = "${data.aws_route53_zone.main.zone_id}"
-  name    = "www.${var.site_url}"
-  type    = "A"
+# TODO: this is busted with CORS errors.
+# resource "aws_route53_record" "www" {
+#   zone_id = "${data.aws_route53_zone.main.zone_id}"
+#   name    = "www.${var.site_url}"
+#   type    = "A"
 
-  alias {
-    name                   = "${aws_cloudfront_distribution.main.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.main.hosted_zone_id}"
-    evaluate_target_health = false
-  }
-}
 
-resource "aws_route53_record" "wwwv6" {
-  zone_id = "${data.aws_route53_zone.main.zone_id}"
-  name    = "www.${var.site_url}"
-  type    = "AAAA"
+#   alias {
+#     name                   = "${aws_cloudfront_distribution.main.domain_name}"
+#     zone_id                = "${aws_cloudfront_distribution.main.hosted_zone_id}"
+#     evaluate_target_health = false
+#   }
+# }
 
-  alias {
-    name                   = "${aws_cloudfront_distribution.main.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.main.hosted_zone_id}"
-    evaluate_target_health = false
-  }
-}
+
+# resource "aws_route53_record" "wwwv6" {
+#   zone_id = "${data.aws_route53_zone.main.zone_id}"
+#   name    = "www.${var.site_url}"
+#   type    = "AAAA"
+
+
+#   alias {
+#     name                   = "${aws_cloudfront_distribution.main.domain_name}"
+#     zone_id                = "${aws_cloudfront_distribution.main.hosted_zone_id}"
+#     evaluate_target_health = false
+#   }
+# }
